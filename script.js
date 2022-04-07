4b3c2fa8f7a64f5afb940f51e5672a2bb444454f
Elya-Zh <elya7us@gmail.com>
2022-04-07 15:06:26 -0500

    every email displayed

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function containerBody(emails){
 
     const partialEmails = emails.slice(start, end);
 
-    console.log('partialEmails:', partialEmails)
+    //console.log('partialEmails:', partialEmails)
 
     pageInfoSpan.innerText = `${start+1} - ${end} of ${emails.length}`;
 
@@ -45,7 +45,7 @@ function containerBody(emails){
         });
         
         let card = `
-            <div class="card" id='card2'>
+            <div class="card" id=${email.senderName} >
                 <input type="checkbox" name="check-${email.id}" id="${email.id}">
                 <span class="material-icons">star_outline</span>
                 <span class="material-icons" style="color: rgb(245, 211, 113);">label_important</span>
@@ -151,36 +151,71 @@ document.body.addEventListener('click', (e) => {
 })
 
 
-console.log('click')
-console.log(clear)
-
 let count = 1
-for(let item of primary)
-item.id = count
-count++
-console.log(primary)
+for(let item of primary) {
+    item.id = count
+    count++
+    //console.log(primary)
+}
 
-// mainContainer.addEventListener('click',(a)=>{
-//     mainContainer.innerHTML = '';
-//   let everyEmail = document.createElement('div');
-//   everyEmail.setAttribute('class','eachDiv');
-//   mainContainer.appendChild(everyEmail)
-//   everyEmail.forEach(function(elem, index, arr){
-//       return 
-//       everyEmail.innerHTML = `<h1>${a.messageTitle}</h1>`
-//     })
-//     console.log(elem)
-//   console.log(a.messageTitle)
-  
-  
-// })
+mainContainer.addEventListener('click',(event)=>{
+   let name = event.target.innerText;
+    mainContainer.innerHTML = '';
+    
+    let everyEmail = document.createElement('div');
 
-// question.addEventListener('click',()=>{
-//     let help = document.createElement('div')
-//     help.setAttribute('class','help')
-//     mainContainer.appendChild(help);
-//     help.style.width = '100px';
-//     help.style.marginTop = '15px'
-//     help.style.marginBottom = '0'
-//     help.style.border = '1px solid red'
-// })
+   let currentMessage = primary.filter(message => {
+   // console.log("name =>" +name,  "sender => " , message.senderName)
+    return name === message.senderName
+   })
+   //console.log(currentMessage);
+    currentMessage.map( el => {
+    everyEmail.setAttribute('class','eachDiv');
+    mainContainer.appendChild(everyEmail)
+    everyEmail.innerHTML = `
+    <div class ='eachDiv_header'>
+    <span class="material-icons" id='back'>
+    arrow_back
+    </span>
+    <span class="material-icons">
+    archive
+    </span>
+    <span class="material-icons">
+report
+</span>
+<span class="material-icons">
+delete
+</span>
+<span class="material-icons">
+mark_email_unread
+</span>
+<span class="material-icons">
+watch_later
+</span>
+<span class="material-icons">
+add_task
+</span>
+<span class="material-icons">
+drive_file_move
+</span>
+<span class="material-icons">
+label
+</span>
+<span class="material-icons">
+more_vert
+</span>
+    </div>
+    <h1>${el.messageTitle}</h1>
+    <h4>${el.senderName}</h4><p>${el.senderEmail}</p>
+    <p>${el.messages[0].message}</p>
+    
+    `
+
+})
+})
+//<img src=${el.messages[0].attachments[0].icon}>
+document.body.addEventListener('click', (e) => {
+    if (e.target.id === "back") {
+        containerBody(primary)
+    }
+})
